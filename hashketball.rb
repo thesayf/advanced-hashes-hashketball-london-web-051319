f5603882d3676dac7e2464bfc75f13bda5d0f55a
Rori Hinds <r_ori@hotmail.co.uk>
2019-05-09 16:40:06 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -189,6 +189,21 @@ end
   # game_hash[:away][:players].keys
   end
   end
+  
+  def big_shoe_rebounds
+  biggest = 0
+  rebounds = 0
+  game_hash.each do |home_away, keys|
+    keys[:players].each do |player|
+      size = player[:shoe]
+      if size > biggest
+        biggest = size
+        rebounds = player[:rebounds]
+      end
+    end
+  end
+  rebounds
+end
 
 
 
